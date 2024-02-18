8b47195219c3666acbc2ed77f634ef0e65ba5da9
User <lalal@gmail.com>
2024-02-18 11:01:24 +0200

    add text Label

--- wally.py
+++ wally.py
@@ -11,6 +11,7 @@ img = PhotoImage(file='Wally (3).png')
 lb = Label(image=img)
 lb.pack()
 
+
 def scale(val):
     img['gamma']=val
 scale=Scale(from_=1, to=10, length=700, orient=HORIZONTAL, command=scale)
