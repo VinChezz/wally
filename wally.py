338dadbf33a37d8d82a790df6a3e0494b3c87e5a
User <lalal@gmail.com>
2024-02-18 11:00:32 +0200

    add text Label

--- wally.py
+++ wally.py
@@ -1,16 +1,18 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('750x555')
+root.geometry('750x650')
 root.title('Kyryl')
 root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
 
+text = Label(text='gamma editor', font='Verdana 20')
+text.pack()
 img = PhotoImage(file='Wally (3).png')
 lb = Label(image=img)
 lb.pack()
 
-def set_gamma(val):
+def scale(val):
     img['gamma']=val
-scale=Scale(from_=1, to=10, length=700, orient=HORIZONTAL, command=set_gamma)
+scale=Scale(from_=1, to=10, length=700, orient=HORIZONTAL, command=scale)
 scale.pack()
 root.mainloop()
