3d97d83dd4dc2706d20fcd9159c996835d27dad8
User <lalal@gmail.com>
2024-02-18 10:21:44 +0200

    add image

--- wally.py
+++ wally.py
@@ -1,8 +1,11 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('1440x718')
+root.geometry('750x485')
 root.title('Kyryl')
 root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
 
+img = PhotoImage(file='Wally (3).png')
+lb = Label(image=img)
+lb.pack()
 root.mainloop()
