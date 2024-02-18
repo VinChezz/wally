a4ad698fba9da33403c9f97d3f1a5b4a4dc2082c
User <lalal@gmail.com>
2024-02-18 10:29:33 +0200

    set_gamma image

--- wally.py
+++ wally.py
@@ -1,11 +1,16 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('750x485')
+root.geometry('750x555')
 root.title('Kyryl')
 root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
 
 img = PhotoImage(file='Wally (3).png')
 lb = Label(image=img)
 lb.pack()
+
+def set_gamma(val):
+    img['gamma']=val
+scale=Scale(from_=1, to=10, length=700, orient=HORIZONTAL, command=set_gamma)
+scale.pack()
 root.mainloop()
